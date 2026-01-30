623d1f07ab7e7f871b1877e2a917917db1344dab
Rinor Muji <rinor.muji@student.uni-pr.edu>
2026-01-30 16:50:03 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
-import "./app.css";
+import "./App.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 
